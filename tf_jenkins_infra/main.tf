942d3d40376ad16d8e1ac1b0de39e91ff6d017c5
Andrii Bondariev <andrii.bondariev@namecheap.com>
2022-06-25 21:18:40 +0300

    feat: jenkins key pairs was added

--- tf_jenkins_infra/main.tf
+++ tf_jenkins_infra/main.tf
@@ -1,8 +1,24 @@
 resource "aws_instance" "Jenkins" {
     ami           = data.aws_ami.amazon_linux.id
     instance_type = "t3.micro"
+    //key_name      = "jenkins-key"
 
     tags = {
     Name = "tf_jenkins_infra"
   }
+}
+
+resource "tls_private_key" "Jenkins_RSA" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "jenkins_key" {
+  key_name   = "jenkins-key"
+  public_key = tls_private_key.Jenkins_RSA.public_key_openssh
+}
+
+resource "local_sensitive_file" "Jenkins_key" {
+    content  = tls_private_key.Jenkins_RSA.private_key_pem
+    filename = "jenkins-key"
 }
