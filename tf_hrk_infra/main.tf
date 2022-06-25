97b2952d17f4a77084fd1d2ffebe8d51c25ff099
Andrii Bondariev <andrii.bondariev@namecheap.com>
2022-06-25 23:37:11 +0300

    feat: added 22 port to ingress

--- tf_hrk_infra/main.tf
+++ tf_hrk_infra/main.tf
@@ -29,6 +29,15 @@ resource "aws_security_group" "hrk_SG" {
   description = "for testing"
 
   ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  ingress {
     description      = "HTTP from VPC"
     from_port        = 80
     to_port          = 80
--- tf_jenkins_infra/main.tf
+++ tf_jenkins_infra/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "Jenkins" {
-    ami           = data.aws_ami.amazon_linux.id
-    instance_type = "t3.micro"
-    //key_name      = "jenkins-key"
+    ami                    = data.aws_ami.amazon_linux.id
+    instance_type          = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.Jenkins_SG.id]
+    key_name      = "jenkins-key"
 
     tags = {
     Name = "tf_jenkins_infra"
@@ -21,4 +22,48 @@ resource "aws_key_pair" "jenkins_key" {
 resource "local_sensitive_file" "Jenkins_key" {
     content  = tls_private_key.Jenkins_RSA.private_key_pem
     filename = "jenkins-key"
+}
+
+resource "aws_security_group" "Jenkins_SG" {
+  name        = "Jenkins-SG"
+  description = "for testing"
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Jenkins http/s"
+  }
 }
